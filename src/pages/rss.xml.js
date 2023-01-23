847935c514f43cd9cc3f1cd1772f41b9a3ca6dac
maloong <maloong2022@gmail.com>
2023-01-23 17:37:40 +0800

    feat(add  some function): add some function

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+
+export async function get() {
+  return rss({
+    title: "Maloong Learner | Blog",
+    description: "My journey learning Astro",
+    site: "https://fantastic-parfait-e962c7.netlify.app",
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    customData: `<language>en-us</language>`,
+  });
+}
